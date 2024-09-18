f3ad6a03b5cfee3b9483fa536fd191bbf71d4582
mmussatto <murilomussatto@gmail.com>
2024-09-18 14:07:45 -0300

    Add configuration for SQLite database

--- RealTimeAppClient/src/config.js
+++ RealTimeAppClient/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-	apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "http://localhost:5003",
+	apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "http://localhost:5173",
 };
 
 export default config;
