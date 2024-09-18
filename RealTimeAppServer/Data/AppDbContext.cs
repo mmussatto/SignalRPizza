b1ad3b79c7b5df8efd5ce5ca30a52715cbec1002
mmussatto <murilomussatto@gmail.com>
2024-09-18 14:06:22 -0300

    Remove unused code

--- RealTimeAppServer/Data/AppDbContext.cs
+++ RealTimeAppServer/Data/AppDbContext.cs
@@ -8,7 +8,6 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options) { }
 
-    public DbSet<DataItem> DataItems { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<Customer> Customers { get; set; }
     public DbSet<ChatMessage> ChatMessages { get; set; }
--- RealTimeAppServer/Models/DataItem.cs
+++
@@ -1,7 +0,0 @@
-namespace RealTimeAppServer.Models;
-
-public class DataItem
-{
-    public int Id { get; set; }
-    public required string Data { get; set; }
-}
